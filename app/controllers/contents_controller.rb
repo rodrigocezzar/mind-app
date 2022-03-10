45ec620a9e73f9b15408025c885eab8bce2edfdc
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-03-10 20:25:18 -0300

    feat: update and show content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -7,6 +7,10 @@ class ContentsController < ApplicationController
     @contents = current_user.contents
   end
 
+  def show
+    @content = Content.find(params[:id])
+  end
+
   def new
     @content = Content.new
   end
@@ -15,7 +19,7 @@ class ContentsController < ApplicationController
     @content = current_user.contents.build(content_params)
 
     if @content.save
-      redirect_to contents_path, notice: 'Content successfully created'
+      redirect_to contents_path, notice: 'Content successfully created!'
     else
       render :new
     end
@@ -23,6 +27,16 @@ class ContentsController < ApplicationController
 
   def edit
     @content = Content.find(params[:id])
+  end
+
+  def update
+    @content = Content.find(params[:id])
+
+    if @content.update(content_params)
+      redirect_to contents_path, notice: 'Content successfully updated!'
+    else
+      render :edit
+    end
   end
 
   private
