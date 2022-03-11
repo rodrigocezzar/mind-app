e518476c5a855398d3cea9773686130244b04d81
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-03-11 09:15:42 -0300

    feat: permit add tag in contents

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import $ from "jquery";
+import "select2";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+$(document).on("turbolinks:load", function () {
+  $(".select2").select2({
+    tags: true,
+    tokenSeparators: [",", " "],
+  });
+});
